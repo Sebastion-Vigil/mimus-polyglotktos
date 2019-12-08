815ed3a42273eb6b9f2b22010896ccab9b620451
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-12-08 16:31:03 -0600

    42

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                What up freak bitches?
+            </div>
+        )
+    }
+}
+
+export default App;
