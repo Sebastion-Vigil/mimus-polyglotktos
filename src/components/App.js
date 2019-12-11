b26320f6c2051c81ca6c596655bd35e61a0c7d0e
link <irishseanvaldivia@gmail.com>
2019-12-10 22:30:04 -0600

    working example

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                What up freak bitches?
-            </div>
-        )
-    }
+const Message = props => <div>{props.msg}</div>;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Message msg="A Generic Hello World Text" />
+        <Message msg="Creating Reusable React Components" />
+      </div>
+    );
+  }
 }
 
 export default App;
